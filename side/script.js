ef2ec6af87be30068377657c3510d9b5005455a4
Muhammad Usman <usmansaleem4446996@gmail.com>
2024-01-08 09:40:55 +0500

    Adding section completed

--- side/script.js
+++ side/script.js
@@ -3,3 +3,8 @@ let menuList = document.querySelector(".menu-list");
 function menu() {
     menuList.classList.toggle("h-0");
 }
+
+//automatically adjusts height of form frame
+function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+}
