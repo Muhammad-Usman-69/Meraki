97d031382aeeeb8d906f5934ab34a7517a163658
Muhammad Usman <usmansaleem4446996@gmail.com>
2024-01-11 22:47:02 +0500

    header menu improved

--- side/script.js
+++ side/script.js
@@ -1,7 +1,20 @@
 // adding showing function for menu list
-let menuList = document.querySelector(".menu-list");
+let menuSec = document.querySelector(".menu");
+let menuList = document.querySelectorAll(".menu li");
 function menu() {
-    menuList.classList.toggle("h-0");
+    menuList.forEach((list, i) => {
+        list.classList.toggle("h-9");
+        list.style.transitionDelay = i * 0.1 + "s";
+    });
+
+    let menuImage = document.querySelector(".menu-img");
+    let closeImage = document.querySelector(".close-img");
+
+    //adding rotating function for menu
+    /* menuImage.classList.toggle("opacity-0");
+    menuImage.style.transform = "rotate(180deg)";
+    closeImage.classList.toggle("opacity-0");
+    closeImage.style.transform = "rotate(180deg)"; */
 }
 
 //automatically adjusts height of form frame
